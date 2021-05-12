51db5c223ba986a477984e5cc17c93a2eb5fe9b5
Martins Dreimanis <dreimanis.maartins@gmail.com>
2021-05-12 22:06:18 +0300

    Methodification and renaming variables

--- src/test/java/TestHomework.java
+++ src/test/java/TestHomework.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,65 +12,58 @@ import java.util.List;
 
 public class TestHomework {
     private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[@mode ='primary']");
-    private final By HEADINGS = By.className("list-article__headline");
-    private final By STORY_COMMENTS_BTN = By.xpath(".//a[6]/span [@class= 'article-share__image-container social-button']");
-    //private final By STORY_COMMENTS_BTN = By.className("article-share__item article-share__item--comments article-share__item-with-count");
+    private final By ARTICLES = By.className("list-article__headline");
+    private final By ARTICLE_COMMENTS_BTN = By.xpath(".//a[6]/span[@class= 'article-share__image-container social-button']");
 
-    // homework task #3
-    /*
-    private final By HEADINGS = By.className("list-article__headline");
-    private final By COMMENTS_COUNT = By.className("list-article__comment");
-    private final By LOGO = By.className("flex header-logo");
-    private final By LANG_BTN_RUS = By.xpath(".//div[5]/a [@class ='menu-item']");
-    */
+    WebDriver driver;
+
+    //------------------- METHODS START---------------------------
+    public void initiateTestForWebsite(){
+        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://tvnet.lv");
+    }
+    public void clickCookiesButton(){
+        WebDriverWait wait = new WebDriverWait(driver, 10, 1000);
+        wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
+        driver.findElement(ACCEPT_COOKIES_BTN).click();
+    }
+    //------------------- METHODS END ----------------------------
 
     @Test
     public void homeworkTaskOne() {
-        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://tvnet.lv");
+        initiateTestForWebsite();
+        clickCookiesButton();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10, 1000);                                                               //Accept cookies
-        wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
-        driver.findElement(ACCEPT_COOKIES_BTN).click();
-
-        driver.findElement(HEADINGS).click();                                                                                                            //Interact with main headline
-        driver.findElement(STORY_COMMENTS_BTN).click();                                                                                                  //Move to comments section
+        driver.findElement(ARTICLES).click();            //Interact with main headline
+        driver.findElement(ARTICLE_COMMENTS_BTN).click();  //Move to comments section
     }
 
     @Test
     public void homeworkTaskTwo() {
-        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://tvnet.lv");
+        initiateTestForWebsite();
+        clickCookiesButton();
 
-
-        WebDriverWait wait = new WebDriverWait(driver, 10, 1000);                                                               //Accept cookies
-        wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
-        driver.findElement(ACCEPT_COOKIES_BTN).click();
-
-        //String text = driver.findElement(HEADINGS).getText();                                                                                          //print text contained by the element v1
-        System.out.println("Main headline title is: " + driver.findElement(HEADINGS).getText());                                                         //print text contained by the element v2
+        System.out.println("Main headline title is: " + driver.findElement(ARTICLES).getText());
     }
 
     @Test
     public void homeworkTaskThree() {
-       //at variables ^UP^
+        /*
+        private final By HEADINGS = By.className("list-article__headline");
+        private final By COMMENTS_COUNT = By.className("list-article__comment");
+        private final By LOGO = By.className("flex header-logo");
+        private final By LANG_BTN_RUS = By.xpath(".//div[5]/a [@class ='menu-item']");
+        */
     }
 
     @Test
     public void homeworkTaskFour() {
-        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://tvnet.lv");
+        initiateTestForWebsite();
+        clickCookiesButton();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10, 1000);                                                                //Accept cookies
-        wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
-        driver.findElement(ACCEPT_COOKIES_BTN).click();
-        List<WebElement> headingList = driver.findElements(HEADINGS);                                                                                    //gather(list) all headings
+        List<WebElement> headingList = driver.findElements(ARTICLES);                                                                                    //gather(list) all headings
 
         /*
         //----------------- PRINT WITH COMMENTS (plain) ------------------
@@ -105,7 +99,7 @@ public class TestHomework {
             if (headingList.get(i).findElements(By.xpath(".//span [contains (@class, 'list-article__comment')]")).isEmpty()) {
                 System.out.println("Heading: \"" + headingText + "\" has no comments!");                                                                //if no comments - print as is
             } else {
-                WebElement childElement = headingList.get(i).findElement(By.xpath(".//span [contains (@class, 'list-article__comment')]"));             //check if comments exist
+                WebElement childElement = headingList.get(i).findElement(By.xpath(".//span [contains(@class, 'list-article__comment')]"));              //check if comments exist
                 String childContent = childElement.getText();                                                                                           //"single out" the count of comments and brackets
                 StringBuilder sb = new StringBuilder(headingText);
                 sb.setLength (sb.length() - childContent.length() -1);                                                                                  //cut off comments and the space before comments
@@ -117,6 +111,10 @@ public class TestHomework {
     @Test
     public void homeworkTaskFive()  {
 
+    }
+    @AfterEach
+    public void closeBrowser(){
+        driver.close();
     }
 }
 
