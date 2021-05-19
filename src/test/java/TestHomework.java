74d9b37e5142a1b6ec1a8277bcbd451067c43f05
Martins Dreimanis <dreimanis.maartins@gmail.com>
2021-05-19 21:00:05 +0300

    strugglin'

--- src/test/java/TestHomework.java
+++ src/test/java/TestHomework.java
@@ -15,7 +15,7 @@ public class TestHomework {
     private final By ARTICLES = By.className("list-article__headline");
     private final By ARTICLE_COMMENTS_BTN = By.xpath(".//a[6]/span[@class= 'article-share__image-container social-button']");
     private final By COMMENTS_COUNT = By.xpath(".//span [contains (@class, 'list-article__comment')]");
-    private final By PRE_ARTICLE_EXC = By.className("list-article__headline--exclamation");
+    private final By ARTICLE_EXCLAMATION = By.className("list-article__headline--exclamation");
 
     WebDriver driver;
     //------------------- METHODS START---------------------------
@@ -35,8 +35,8 @@ public class TestHomework {
         return value;
     }
     public String articleTitleCleanup(List<WebElement> list, int index){
-        String clearExclamation = list.get(index).findElements(PRE_ARTICLE_EXC).isEmpty() ? list.get(index).getText()
-                : list.get(index).getText().substring(list.get(index).findElement(PRE_ARTICLE_EXC).getText().length() +1);
+        String clearExclamation = list.get(index).findElements(ARTICLE_EXCLAMATION).isEmpty() ? list.get(index).getText()
+                : list.get(index).getText().substring(list.get(index).findElement(ARTICLE_EXCLAMATION).getText().length() +1);
         String clearComments = checkIfNoCommentsExist(list, index) ? clearExclamation
                 : clearExclamation.substring(0, clearExclamation.length() - list.get(index).findElement(COMMENTS_COUNT).getText().length() -1);
         return clearComments;
@@ -78,6 +78,8 @@ public class TestHomework {
         List<WebElement> articleList = driver.findElements(ARTICLES);
         for (int i = 0; i < articleList.size(); i++) {
             System.out.println((i+1) + " " + articleTitleCleanup(articleList, i));
+
+            //System.out.println(articleList.get(110).findElement(COMMENTS_COUNT).getText().isEmpty());
         }
     }
 
@@ -86,25 +88,44 @@ public class TestHomework {
         initiateTestForWebsite();
         clickCookiesButton();
 
-        List<WebElement> articleList = driver.findElements(ARTICLES);     //gather(list) all articles
+        List<WebElement> articleList = driver.findElements(ARTICLES);
 
 
         for (int i = 0; i < articleList.size(); i++) {
-            String headingText = articleTitleCleanup(articleList, i);
+            String articleText = articleTitleCleanup(articleList, i);
+            int commentsCount = 0;
+
+            System.out.print( i + 1 + articleText);
+            commentsCount = checkIfNoCommentsExist(articleList, i) || articleList.get(i).findElement(COMMENTS_COUNT).getText().length() == 0 ? 0
+            :Integer.parseInt(articleList.get(i).findElement(COMMENTS_COUNT).getText().substring(1, articleList.get(i).findElement(COMMENTS_COUNT).getText().length() - 1));
+            System.out.println(" --- " + commentsCount);
+        }
+
+    }
+    @Test
+    public void Extra()  {
+        initiateTestForWebsite();
+        clickCookiesButton();
+
+        List<WebElement> articleList = driver.findElements(ARTICLES);
+
+
+        for (int i = 0; i < articleList.size(); i++) {
+            String articleText = articleTitleCleanup(articleList, i);
 
             if (checkIfNoCommentsExist(articleList, i)) {
-                System.out.println("Article: \"" + headingText + "\" has no comments!");
+                System.out.println((i +1) + "Article: \"" + articleText + "\" has no comments!");
             }else {
                 WebElement childElement = articleList.get(i).findElement(COMMENTS_COUNT);
                 String childContent = childElement.getText();
-                System.out.println("Article: \"" + headingText + "\" has " + childContent.substring(1, childContent.length() -1) + " comments!");
+                System.out.println((i +1) + "Article: \"" + articleText + "\" has " + childContent.substring(1, childContent.length() -1) + " comments!");
             }
         }
 
     }
     //--------------------- TESTS END ----------------------------
     @AfterEach
-    public void closeBrowser(){
-        driver.close();
-    }
+    public void closeBrowser(){ driver.close(); }
 }
+//homework - add more methods
+//homework - add logs
