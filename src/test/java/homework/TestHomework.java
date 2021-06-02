f0b219baabb7bf30f0f89f34e1340c7a38123c4c
Martins Dreimanis <dreimanis.maartins@gmail.com>
2021-06-02 14:26:16 +0300

    Update for tvnet test again, removing unused imports and a reminder to actually implement logs properly somewhere in the future

--- src/test/java/homework/TestHomework.java
+++ src/test/java/homework/TestHomework.java
@@ -7,9 +7,7 @@ import homework.pages.HomePage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
